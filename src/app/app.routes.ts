595ae6e4598c0b065946b9b239f5791579f163e0
Chloé PAULIN <chloe.previl@gmail.com>
2025-08-30 00:19:00 +0200

    Face-Snap component bouton ajouté avec class et modification automatique

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { FaceSnap } from './face-snap/face-snap';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'facesnap',
+        component: FaceSnap
+    },
+    {
+        path: '',
+        component: FaceSnap,
+        pathMatch: 'full'
+    }
+];
--- src/app/face-snap/face-snap.ts
+++ src/app/face-snap/face-snap.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './face-snap.html',
   styleUrl: './face-snap.css'
 })
-export class FaceSnap {
 
+export class FaceSnap implements OnInit {
+  title!: string;
+  description!: string;
+  createdAt!: Date;
+  snaps!: number;
+  // Snaps = nombre de like
+  imageUrl!: string;
+  snapped!: string;
+  snappedContent!: string;
+
+  ngOnInit(): void {
+    this.title = 'Archibald';
+    this.description = 'Mon meilleur ami depuis toujours !';
+    this.createdAt = new Date();
+    this.snaps = 0
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.snappedContent = "Oh Snap !";
+    this.snapped = "";
+  };
+
+  onAddSnap(): void {
+    if (this.snapped === "") {
+      this.snaps++;
+      this.snapped = "snapped";
+      this.snappedContent = "Oops, un Snap !"
+    } else {
+      if (this.snaps > 0) {
+        this.snaps--;
+        this.snapped = "";
+        this.snappedContent = "Oh Snap !";
+      }
+    }
+  }
 }
